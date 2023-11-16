78cec7f4669933951a8e81c5f09f81d5cb6ec42a
MzMedia <mzmedia2023@126.com>
2023-11-16 18:26:14 +0800

    1.更新文档

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
 
-push_addr=git@github.com:Mzmedia/Mzmedia.github.io.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
-
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd $dist_path
-
+# deploy to github
+#echo 'blog.xugaoyi.com' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:Mzmedia/Mzmedia.github.io.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://Mzmedia:${GITHUB_TOKEN}@git@github.com:Mzmedia/Mzmedia.github.io.git
+  git config --global user.name "MzMedia"
+  git config --global user.email "mzmedia2023@126.com"
+fi
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
-
-cd -
-rm -rf $dist_path
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
