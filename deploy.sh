75016bbc5e54357fe595890a118f1f6c6f38ec68
MzMedia <mzmedia2023@126.com>
2023-11-16 18:34:05 +0800

    1.更新文档

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ npm run build # 生成静态文件
 cd docs/.vuepress/dist # 进入生成的文件夹
 
 # deploy to github
+git config --global user.name "MzMedia"
+git config --global user.email "mzmedia2023@126.com"
+
 #echo 'blog.xugaoyi.com' > CNAME
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
@@ -12,10 +15,13 @@ if [ -z "$GITHUB_TOKEN" ]; then
 else
   msg='来自github action的自动部署'
   githubUrl=https://Mzmedia:${GITHUB_TOKEN}@git@github.com:Mzmedia/Mzmedia.github.io.git
-  git config --global user.name "MzMedia"
-  git config --global user.email "mzmedia2023@126.com"
+
 fi
+
 git init
 git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
+
+cd -
+rm -rf docs/.vuepress/dist
